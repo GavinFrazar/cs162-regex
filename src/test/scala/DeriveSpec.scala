ed1c865aaf497b2cd9543cbf850c5d2ce463a3a6
Gavin Frazar <gavinfrazar@gmail.com>
2018-10-25 01:46:50 -0700

    Add tests for DerivativeMachine

--- src/test/scala/DeriveSpec.scala
+++ src/test/scala/DeriveSpec.scala
@@ -47,47 +47,119 @@ class DeriveSpec extends FlatSpec with Matchers {
   it should "recognize strings in Kleenestar(b)" in {
     (Derive.matches(b.*, "bbbbbbbbbb")
        && Derive.matches(b.*, "")
-    ) should be (true)
+    ) should equal (true)
   }
 
   it should "recognize the empty string" in {
-    Derive.matches(ε, "") should be (true)
+    Derive.matches(ε, "") should equal (true)
   }
 
   it should "recognize strings in re1 ~ re2" in {
-    Derive.matches(re1 ~ re2, "ad") should be (true)
+    Derive.matches(re1 ~ re2, "ad") should equal (true)
   }
   it should "recognize strings that are in the complement of re2" in {
-    Derive.matches(!re2, "zyxwutq") should be (true)
+    Derive.matches(!re2, "zyxwutq") should equal (true)
   }
 
   it should "recognize strings that are in re_set & re2" in {
     (Derive.matches(re_set & re2, "c")
        && Derive.matches(re_set & re2, "d")
-    ) should be (true)
+    ) should equal (true)
   }
 
   // more tests...
 
   it should "not recognize strings not in the language 1" in {
-    Derive.matches(b, "z") should be (false)
+    Derive.matches(b, "z") should equal (false)
   }
 
   it should "not recognize strings not in the language 2" in {
-    Derive.matches(b ~ c, "b") should be (false)
+    Derive.matches(b ~ c, "b") should equal (false)
   }
 
   it should "not recognize strings not in the language 3" in {
-    Derive.matches(b | c, "e") should be (false)
+    Derive.matches(b | c, "e") should equal (false)
   }
 
   it should "not recognize strings not in the language 4" in {
-    Derive.matches(!b, "b") should be (false)
+    Derive.matches(!b, "b") should equal (false)
   }
 
   it should "not recognize strings not in the language 5" in {
-    Derive.matches(c.* & c, "") should be (false)
+    Derive.matches(c.* & c, "") should equal (false)
   }
 
   // more tests...
+
+  import edu.ucsb.cs.cs162.regex.derivative._
+
+  behavior of "eval"
+
+
+  it should "recognize strings that are in a set" in {
+    (DerivativeMachine(re_set).eval("a")
+       && DerivativeMachine(re_set).eval("b")
+       && DerivativeMachine(re_set).eval("c")
+       && DerivativeMachine(re_set).eval("d")
+    ) should equal (true)
+  }
+
+  it should "recognize strings of concatenated letters" in {
+    DerivativeMachine("abcd".concatenate).eval("abcd") should equal (true)
+  }
+
+  it should "recognize strings that are in the Union of re1 and re2" in {
+    val r = re1 | re2
+    (DerivativeMachine(r).eval("a")
+       && DerivativeMachine(r).eval("b")
+       && DerivativeMachine(r).eval("ccccd")
+       && DerivativeMachine(r).eval("dddd")
+    ) should equal (true)
+  }
+  
+  it should "recognize strings in Kleenestar(b)" in {
+    (DerivativeMachine(b.*).eval("bbbbbbbbbb")
+       && DerivativeMachine(b.*).eval("")
+    ) should equal (true)
+  }
+
+  it should "recognize the empty string" in {
+    (DerivativeMachine(ε).eval("")) should equal (true)
+  }
+
+  it should "recognize strings in re1 ~ re2" in {
+    (DerivativeMachine(re1 ~ re2).eval("ad")) should equal (true)
+  }
+  it should "recognize strings that are in the complement of re2" in {
+    (DerivativeMachine(!re2).eval("zyxwutq")) should equal (true)
+  }
+
+  it should "recognize strings that are in re_set & re2" in {
+    (DerivativeMachine(re_set & re2).eval("c")
+       && DerivativeMachine(re_set & re2).eval("d")
+    ) should equal (true)
+  }
+
+  // more tests...
+
+  it should "not recognize strings not in the language 1" in {
+    (DerivativeMachine(b).eval("z")) should equal (false)
+  }
+
+  it should "not recognize strings not in the language 2" in {
+    (DerivativeMachine(b ~ c).eval("b")) should equal (false)
+  }
+
+  it should "not recognize strings not in the language 3" in {
+    (DerivativeMachine(b | c).eval("e")) should equal (false)
+  }
+
+  it should "not recognize strings not in the language 4" in {
+    (DerivativeMachine(!b).eval("b")) should equal (false)
+  }
+
+  it should "not recognize strings not in the language 5" in {
+    (DerivativeMachine(c.* & c).eval("")) should equal (false)
+  }
+
 }
