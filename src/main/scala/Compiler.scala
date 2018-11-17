23f9f4e9b0ed89bf90ecbd499b8bc336d07b7033
Gavin Frazar <gavinfrazar@gmail.com>
2018-11-17 02:43:54 -0800

    Fix jump offset

--- src/main/scala/Compiler.scala
+++ src/main/scala/Compiler.scala
@@ -21,8 +21,24 @@ object Compiler {
         val left = compile_helper(l) :+ PushLeft
         val right = compile_helper(r) :+ PushRight
         ((Fork(1, left.length+2) +: left) ++
-           (Jump(right.length) +: right))
+           (Jump(right.length + 1) +: right))
       }
+      case KleeneStar(re) => {
+        val stub = re.nullable match {
+          case `ε` => {
+            val inside = compile_helper(re)
+            IndexedSeq(CheckProgress, Fork(1, inside.length+3)) ++ inside ++
+              IndexedSeq(PushStar, Jump(-1*(inside.length+3)))
+          }
+          case `∅` => {
+            val inside = compile_helper(re)
+            IndexedSeq(Fork(1, inside.length+3)) ++ inside ++
+            IndexedSeq(PushStar,Jump(-1*(inside.length+2)))
+          }
+        }
+        InitStar +: stub
+      }
+      case Capture(_, re) => compile_helper(re)
       case _ => IndexedSeq(Reject)
     }
     compile_helper(re) :+ Accept
